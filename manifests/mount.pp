db9d9ae46e1b86034bc044dac9d3d84805b10b4f
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-01-29 16:35:23 +0100

    Allow distinguishing between gluster::mount title and path to avoid double entries in fstab
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/mount.pp
+++ manifests/mount.pp
@@ -19,23 +19,24 @@ define glusterfs::mount (
 
   if $fstype == 'glusterfs' {
     mount { $path:
-      ensure  => $ensure,
-      device  => $device,
-      fstype  => $fstype,
-      options => $options,
-      require => Package['glusterfs-fuse'],
+      ensure   => $ensure,
+      device   => $device,
+      fstype   => $fstype,
+      options  => $options,
+      remounts => false,
+      require  => Package['glusterfs-fuse'],
     }
   }
   elsif $fstype == 'nfs' {
     mount { $path:
-      ensure  => $ensure,
-      device  => $device,
-      fstype  => $fstype,
-      options => $options,
+      ensure   => $ensure,
+      device   => $device,
+      fstype   => $fstype,
+      remounts => false,
+      options  => $options,
     }
   }
   else {
     fail("${fstype} is not a valid filesystem for gluster")
   }
 }
-
