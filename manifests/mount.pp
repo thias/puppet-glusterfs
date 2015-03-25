7cd9a0b0a4885ada69056c25c680345c771c361e
Matthias Kneer <matze@roterruler.de>
2015-03-25 23:27:44 +0100

    Added the possibility to change the fstype if required to *nfs'. Default is 'glusterfs'.

--- manifests/mount.pp
+++ manifests/mount.pp
@@ -10,18 +10,31 @@
 define glusterfs::mount (
   $device,
   $options = 'defaults,_netdev',
+  $fstype  = 'glusterfs',
   $ensure  = 'mounted'
 ) {
 
   include glusterfs::client
 
-  mount { $title:
-    ensure  => $ensure,
-    device  => $device,
-    fstype  => 'glusterfs',
-    options => $options,
-    require => Package['glusterfs-fuse'],
+  if $fstype == 'glusterfs' {
+    mount { $title:
+      ensure  => $ensure,
+      device  => $device,
+      fstype  => $fstype,
+      options => $options,
+      require => Package['glusterfs-fuse'],
+    }
   }
-
+  elseif $fstype == 'nfs' {
+    mount { $title:
+      ensure  => $ensure,
+      device  => $device,
+      fstype  => $fstype,
+      options => $options,
+    }
+  }
+  else {
+    fail("$fstype is not a valid filesystem for gluster")
+  }
 }
 
