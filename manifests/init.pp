daa9a07620f51ba0fd567389b4eb63e7d17baf13
Jan Čapek <jan.capek@braiins.cz>
2016-01-12 17:55:55 +0100

    Add base class that configures package repositories for a Debian distro

---
+++ manifests/init.pp
@@ -0,0 +1,55 @@
+# == Class: glusterfs
+#
+# Sets up basic packaging for Debian
+#
+# === Parameters
+#
+# [*major*]
+#   major version for glusterfs
+# [*minor*]
+#   minor version for glusterfs
+# [*release*]
+#   release number
+# [*gpg_key_id*]
+#   optional GPG key ID
+#
+# === Examples
+#
+# class { 'glusterfs':
+#  $major    => '3',
+#  $minor    => '7',
+#  $release  => '6',
+# }
+#
+# === Authors
+#
+# Braiins Systems s.r.o.
+#
+# === Copyright
+#
+# Copyright 2016 Braiins Systems s.r.o.
+#
+class glusterfs (
+  $major,
+  $minor,
+  $release,
+  $gpg_key_id='D5DC52DC',
+) {
+
+  $maj_min = "${major}.${minor}"
+  $url_base = "http://download.gluster.org/pub/gluster/glusterfs/${maj_min}/${maj_min}.${release}"
+  apt::key { 'D5DC52DC':
+    key_source => "${url_base}/pub.key"
+  } ->
+  apt::source { 'gluster':
+    location    => "${url_base}/Debian/${lsbdistcodename}/apt",
+    release     => $lsbdistcodename,
+    repos       => 'main',
+    include_src => false,
+  }
+  apt::pin { 'gluster':
+    priority => '700',
+    packages => 'gluster*',
+    codename  => $lsbdistcodename,
+  }
+}
